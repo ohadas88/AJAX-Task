433d227b0fd15cbb0378e616123b241dfd057807
ohad <ohadas88@gmail.com>
2020-07-15 13:03:03 +0300

    returning the right country

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log(data);
+const cardsContainer = $("#cardsContainer");
+const btn = $("#getCountriesBtn");
+const input1 = $("#countryInput");
+
+btn.on("click", function () {
+  const country = data.find(
+    (country) => input1.val().toLowerCase() == country.name.toLowerCase()
+  );
+  console.log(country);
+});
+
+function getSingleCard(country) {
+  const cardContainer = $(`<div class="card">${country}</div>`);
+  console.log(cardContainer);
+}
+
+{
+  /* <div class="card">
+  <img src="img_avatar.png" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>John Doe</b></h4>
+    <p>Architect & Engineer</p>
+  </div>
+</div> */
+}
+
+function draw(array) {}
