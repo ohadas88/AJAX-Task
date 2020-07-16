2eb70b5400ba0cb2bf829becdaa098385cd4711c
ohad <ohadas88@gmail.com>
2020-07-16 08:50:04 +0300

    completed without AJAX

--- app.js
+++ app.js
@@ -6,22 +6,25 @@ btn.on("click", function () {
   const country = data.find(
     (country) => input1.val().toLowerCase() == country.name.toLowerCase()
   );
-  console.log(country);
+
+  _draw();
+
+  function _draw() {
+    cardsContainer.append(
+      getSingleCard(country.name, country.population, country.flag)
+    );
+  }
 });
 
-function getSingleCard(country) {
-  const cardContainer = $(`<div class="card">${country}</div>`);
-  console.log(cardContainer);
+function getSingleCard(countryName, population, flag) {
+  return $(`<div class="card">
+  <img src=${flag} style="width:100%">
+  <div class="container">
+  <h4><b>${countryName}</b></h4>
+  <p>Number of citizens:${population}</p>
+  </div>  
+  </div>`);
 }
 
 {
-  /* <div class="card">
-  <img src="img_avatar.png" alt="Avatar" style="width:100%">
-  <div class="container">
-    <h4><b>John Doe</b></h4>
-    <p>Architect & Engineer</p>
-  </div>
-</div> */
 }
-
-function draw(array) {}
